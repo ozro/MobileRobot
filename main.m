19127937f777f02458000525aabfd89f3b63621c
jnan1 <jnan1@andrew.cmu.edu>
2016-10-01 17:45:56 -0400

    error calculated, have not yet converted to body coordinates

--- main.m
+++ main.m
@@ -4,7 +4,63 @@ traj = RobotTrajectory(0, tf, ref);
 robot = NohBot();
 
 t = 0;
+prevt = 0;
+sTime = tic;
+realPose = zeros(3,1);
+
+global encoderData;
+global encoderTime;
+
+prevEnc = encoderData;
+enc = encoderData;
+prevT = encoderTime;
+T = encoderTime;
+
+x = 0;
+y = 0;
+b = 0;
+v = 0;
+w = 0;
+prevV = 0;
+prevW = 0;
 while(t<tf)
+    if(t == 0)
+        t = toc(sTime);
+        continue;
+    end
+    t = toc(sTime);
+    dt = t- prevt;
+    prevt = t;
+    
+    T = encoderTime;
+    enc = encoderData;
+    if(T == prevT)
+        pause(0.0001)
+        continue;
+    end
+    dT = T - prevT;
+    prevT = T;
+    
+    vl = (enc(1) - prevEnc(1))/ dT;
+    vr = (enc(2) - prevEnc(2))/ dT;
+    
+    [v,w] = robot.wheelToAngVel(vl,vr);
+    
+    v1 = (prevV + v) / 2;
+    w1 = (prevW + w) / 2;
+    
+    b = b + w1 * dt/2;
+    x = x + v * cos(b) * dt;
+    y = y + v * sin(b) * dt;
+    b = b + w1 * dt/2;
+    
+    prevV = v;
+    prevW = w;
+    
+    realPoseW = [x, y, b];
+    refPoseW = traj.getPose(t);
+    
+    ePoseW = refPoseW - realPoseW;
     
 end
 
