4fa887c6ec748a30a4f6e9af4e46bf859d1abf15
angelica1997 <zhixin.feng@gmail.com>
2016-10-15 19:20:02 -0400

    lab 7 final

--- Classes/mrplSystem.m
+++ Classes/mrplSystem.m
@@ -1,16 +1,32 @@
-classdef mrplSystem
+classdef mrplSystem<handle
     %MRPLSYSTEM Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
         robot
         feedback
+        pose
+        finalPose
+        timeArray
+        refArray
+        realArray
+        errorArray
+        index
     end
     
     methods 
         function obj = mrplSystem(Nobot,feedback)
             obj.robot = Nobot;
             obj.feedback = feedback;
+            obj.pose = zeros(1,3);
+            obj.finalPose = zeros(1,3);
+                        
+            obj.timeArray = zeros(10000,1);
+            obj.refArray = zeros(10000, 3);
+            obj.realArray = zeros(10000, 3);
+            obj.errorArray = zeros(10000, 3);
+            obj.index = 1;
+            
         end
         
         function executeTrajectory(obj,curve)
@@ -25,21 +41,27 @@ classdef mrplSystem
 
             prevEnc = encoderData;
             prevrealT = encoderTime;
-
-            x = 0;
-            y = 0;
-            b = 0;
+            
+            if(obj.index == 1) 
+                x = 0;
+                y = 0;
+                b = 0;
+            else
+                x = obj.realArray(obj.index-1, 1);
+                y = obj.realArray(obj.index-1, 2);
+                b = obj.realArray(obj.index-1, 3);
+            end
             prevV = 0;
             prevW = 0;
+
             
-            timeArray = zeros(1,1);
-            refArray = zeros(1, 3);
-            realArray = zeros(1, 3);
-            errorArray = zeros(1, 3);
-            
-            prevPos = zeros(2,1);
-            prevB = 0;
+            prevPos = [x; y];
+            prevB = b;
             prevt = 0;
+            
+            tran = [cos(obj.pose(3)), -sin(obj.pose(3)), obj.pose(1); 
+                    sin(obj.pose(3)), cos(obj.pose(3)), obj.pose(2); 0,0,1];
+
             while(t<tf + 0.25)
                 if(t == 0)
                     t = toc(start);
@@ -78,32 +100,35 @@ classdef mrplSystem
                 b = b + avgW * realdt/2;
 
                 realPoseW = [x, y, b]';
-                refPoseW = getPoseAtTime(curve,t-obj.robot.delay);
+                refPoseR = getPoseAtTime(curve,t-obj.robot.delay);
+                refPoseW = tran * [refPoseR(1); refPoseR(2); 1];
+                refPoseW(3) = refPoseR(3) + obj.pose(3);
 
                 ePoseW = refPoseW - realPoseW;
+
+                ePoseW
                 th = refPoseW(3);
-                H = [cos(th), -sin(th); sin(th), cos(th)];
+                H = [cos(th), sin(th); -sin(th), cos(th)];
                 ePosW = [ePoseW(1); ePoseW(2)];
-                ePosR = H\ePosW;
+                ePosR = H*ePosW;
                 ePoseR = [ePosR(1), ePosR(2), ePoseW(3)];
                 if(obj.feedback)
-                    ePos = [ePoseR(1); ePoseR(2)];
                     eb = ePoseR(3);
-                    kx = 0.25;
-                    ky = 0.25;
-                    kb = 0.0020;
+                    kx = 0.005;
+                    ky = 0.005;
+                    kb = 0.0015;
 
                     k = [kx, 0; 0, ky];
-                    u = k * ePos;
+                    u = k * ePosR;
                     eV = u(1);
                     eW = u(2) + eb*kb;
                     
-                    dPos = (ePos - prevPos)/dt;
-                    prevPos = ePos;
-                    deb = (eb - prevB)/dt;
-                    prevB=eb;
+                    dPos = (ePosR-prevPos)/dt;
+                    prevPos = ePosR;
+                    deb = (eb-prevB)/dt;
+                    prevB = eb;
                     
-                    kdx = 0.015;
+                    kdx = 0.0;
                     kdy = 0.0;
                     kdb = 0.0;
                     kd = [kdx, 0; 0, kdy];
@@ -128,35 +153,62 @@ classdef mrplSystem
                 end
                 obj.robot.moveAng(vel, angvel);
                 
-                timeArray(end + 1) = realT - sTime;
-                realArray(end + 1, :) = realPoseW;
-                refArray(end + 1, :) = refPoseW;
-                errorArray(end + 1, :) = ePoseR;
-               
+                obj.timeArray(obj.index) = realT - sTime;
+                obj.realArray(obj.index, :) = realPoseW;
+                obj.refArray(obj.index, :) = refPoseW;
+                obj.errorArray(obj.index, :) = ePoseR;
+                obj.index = obj.index +1;
             end
-                obj.robot.stop();
-                
-                figure();
-                plot(refArray(:,1), refArray(:,2), realArray(:,1), realArray(:,2));
-                p = gcf;
-                n = int2str((p.get('Number')+1)/2);
-                title(strcat('Position Graph(', n, ')'));
-                legend('ref','real');
-                xlabel('x (m)');
-                ylabel('y (m)');
-                
-                figure();
-                plot(timeArray, errorArray(:,1),timeArray, errorArray(:,2),timeArray, errorArray(:,3));
-                title(strcat('Error vs Time(', n, ')'));
-                legend('x', 'y', 'th'); 
-                xlabel('time (s)');
-                ylabel('error (m)');
+            obj.robot.stop();
+
+
+
+            figure(1);
+            plot(-obj.refArray(1:obj.index-1,2), obj.refArray(1:obj.index-1,1), -obj.realArray(1:obj.index-1,2), obj.realArray(1:obj.index-1,1));
+            p = gcf;
+            n = int2str((p.get('Number')+1)/2);
+            title(strcat('Position Graph(', n, ')'));
+            legend('ref','real');
+            xlabel('x (m)');
+            ylabel('y (m)');
+
+            figure(2);
+            plot(obj.timeArray(1:obj.index-1), obj.errorArray(1:obj.index-1,1),obj.timeArray(1:obj.index-1), obj.errorArray(1:obj.index-1,2),obj.timeArray(1:obj.index-1),obj.errorArray(1:obj.index-1,3));
+            title(strcat('Error vs Time(', n, ')'));
+            legend('x', 'y', 'th'); 
+            xlabel('time (s)');
+            ylabel('error (m)');
+
+         %  plot(obj.timeArray(1:obj.index-1),obj.refArray(1:obj.index-1,1),obj.timeArray(1:obj.index-1),obj.realArray(1:obj.index-1,1));
+            
+            obj.pose = obj.finalPose;
+        end
+        
+        function executeTrajectoryToAbsPose(obj,tarX,tarY,tarTh,sgn)
+            x = obj.finalPose(1);
+            y = obj.finalPose(2);
+            th = obj.finalPose(3);
+            xp = -(x*cos(th) + y*sin(th));
+            yp = (x*sin(th) - y*cos(th));
+            tran = [cos(th), sin(th), xp; -sin(th), cos(th), yp; 0, 0, 1];
+            
+            relpose = tran*[tarX; tarY; 1];
+            relAng = tarTh - th;
+            
+            relX = relpose(1);
+            relY = relpose(2);
+            relTh = relAng;
+            
+            obj.finalPose = [tarX, tarY, tarTh];
+
+            
+            executeTrajectoryToRelativePose(obj, relX, relY, relTh, sgn);
         end
         
         function executeTrajectoryToRelativePose(obj,x,y,th,sgn)
             curve = cubicSpiral.planTrajectory(x,y,th,sgn);
             curve.planVelocities(0.25);
-            obj.executeTrajectory(curve);         
+            obj.executeTrajectory(curve);
         end
     end
     
